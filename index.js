6bf9a0185463cd9fda4dedff1826f68824b1fc56
Charles von Lehe <vonlehec@gmail.com>
2018-08-27 15:32:39 -0400

    Added thumbnail image, detail text, and album artwork

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 // Import a library to create a component
 import React from 'react';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, View } from 'react-native';
 import Header from './src/components/header';
-
+import AlbumList from './src/components/AlbumList';
 // Create a component
 const App = () => (
-  <Header headerText={'Albums'} />
+  <View style={{ flex: 1 }}>
+    <Header headerText={'Albums'} />
+    <AlbumList />
+  </View>
 );
 
 
